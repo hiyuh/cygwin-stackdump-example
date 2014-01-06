2c9870cbc81cd83c01bb8ca96a654cd96d5152aa
KIMURA Masaru <hiyuh.root@gmail.com>
2014-01-06 16:10:48 +0900

    Support unwind.h.

---
+++ Makefile
@@ -0,0 +1,31 @@
+# NOTE: -funwind-tables is required for clang-3.1, gcc-4.8.2 does not want.
+#       -fasynchronous-unwind-tables is required?
+#       -fexceptions is required?
+#       -g is required to work addr2line properly.
+CFLAGS = -g -O0 -std=gnu99 -Wall -funwind-tables
+
+all: check
+
+test-cygwin_stackdump.exe: test.c
+	$(CC) -DUSE_CYGWIN_STACKDUMP $(CFLAGS) -o $@ $^ $(LDFLAGS) $(LIBS)
+
+test-unwind.exe: test.c
+	$(CC) -DUSE_UNWIND $(CFLAGS) -o $@ $^ $(LDFLAGS) $(LIBS)
+
+.PHONY: info check clean
+
+info:
+	$(CC) -v
+
+check:                        \
+	test-cygwin_stackdump.exe \
+	test-unwind.exe
+	./test-cygwin_stackdump.exe
+	./test-cygwin_stackdump.exe 2>&1 | awk '/^[0-9]/ { print $$2 }' | addr2line -f -e ./test-cygwin_stackdump.exe
+	./test-unwind.exe
+	./test-unwind.exe           2>&1 | awk          '{ print $$2 }' | addr2line -f -e ./test-unwind.exe
+
+clean:
+	rm -f test-cygwin_stackdump.exe
+	rm -f test-cygwin_stackdump.exe.stackdump
+	rm -f test-unwind.exe
--- test.c
+++ test.c
@@ -1,7 +1,71 @@
-extern void cygwin_stackdump();
+#if defined(USE_CYGWIN_STACKDUMP)
+	#include <assert.h>
+	#include <stdio.h>
+	#include <stdbool.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <unistd.h>
+	#include <sys/types.h>
+	#include <sys/wait.h>
+	extern void cygwin_stackdump();
+	static void show_stackdump() {
+		// NOTE: fopen() stackdump file immediately after cygwin_stackdump() calling
+		//       in signle process will fail, so use fork() and waitpid().
+		pid_t pid = fork();
+		if (pid == 0) {
+			cygwin_stackdump();
+			return;
+		} else if (pid > 0){
+			int status;
+			if (waitpid(pid, &status, 0) != pid) {
+				assert(false);
+			}
+			if (WEXITSTATUS(status) != 0) {
+				assert(false);
+			}
+			char *fname = (char *)realloc(
+				strdup(__FILE__),
+				strlen(__FILE__)
+				- strlen(".c")
+				+ strlen("-cygwin_stackdump.exe.stackdump")
+				+ 1
+			);
+			strcpy(&fname[strlen(__FILE__) - strlen(".c")], "-cygwin_stackdump.exe.stackdump");
+			FILE *fp = fopen(fname, "r");
+			assert(fp != NULL);
+			size_t size;
+			char buf[1024];
+			while ((size = fread(buf, sizeof(char), 1024, fp)) > 0)
+				fwrite(buf, sizeof(char), size, stderr);
+			fclose(fp);
+			remove(fname);
+			free(fname);
+			return;
+		} else {
+			assert(false);
+		}
+	}
+#elif defined(USE_UNWIND)
+	// FIXME: cygwin has clang-3.1 as most recent version.
+	//        w/ 3.1, only _Unwind_GetIP() and _Unwind_Backtrace() are supported on !arm.
+	//        as upstream version, see http://clang.llvm.org/doxygen/unwind_8h.html.
+	#include <stdio.h>
+	#include <inttypes.h>
+	#include <unwind.h>
+	_Unwind_Reason_Code trace_fn(struct _Unwind_Context *ctx, void *d) {
+		int *depth = (int *)d;
+		fprintf(stderr, "#%d: %08" PRIxPTR "\n", *depth, _Unwind_GetIP(ctx));
+		(*depth)++;
+		return _URC_NO_REASON;
+	}
+	static void show_stackdump() {
+		int depth = 0;
+		_Unwind_Backtrace(&trace_fn, &depth);
+	}
+#endif
 
 static void f10() {
-	cygwin_stackdump();
+	show_stackdump();
 	return;
 }
 
